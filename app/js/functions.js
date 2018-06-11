74bc6442f7055bc1c1253b41d5deb91cfbec05a9
Denise Buder <d.buder@sbg.at>
2018-06-11 10:32:16 +0200

    error

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,4 +1,23 @@
-const fs = require('fs')
+var solr = require('solr-client')
+
+var client = solr.createClient('localhost', '8983', 'gettingstarted');
+
+var query = client.createQuery()
+				  .q('1902')
+				  .dismax()
+				  .qf({ prim_txt_en: 0.8, start_year_txt_en: 0.2})
+				  .mm(2)
+				  .start(0)
+				  .rows(100);
+
+client.search(query,function(err,obj){
+   if(err){
+   	console.log(err);
+   }else{
+   	console.log(obj);
+   }
+});
+
 
 window.searchForMovies = function(){
   let searchString = document.getElementById("searchInput").value;
